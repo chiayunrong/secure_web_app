9ca2ca71a9f838c2c1ea7c7b84ea430f22d5433a
Adriel0108 <1605136I@student.tp.edu.sg>
2018-02-09 02:00:07 +0800

    having trouble retrieving data

--- admin/admin_home.php
+++ admin/admin_home.php
@@ -1,76 +1,82 @@
-<?php
-$connect=mysqli_connect("localhost","root","","dokidoki");
-
-if(isset($_POST["insert"])){
-	if($_POST["insert"]=="yes")
-	{
-		//$uid=$_POST["UserID"];
-		$username=$_POST["Username"];
-		$password=$_POST["Password"];
-		$name=$_POST["Name"];
-		$role=$_POST["Role"];
-		$contact=$_POST["Contact"];	
-		$email=$_POST["Email"];
-		$creditcardid=$_POST["CreditID"];
-		$query=$connect->prepare("insert into user(Username, Password, Name, Role, Contact, Email, CreditID) values('$username', '$password', '$name', '$role', '$contact', '$email', '$creditcardid');");
-		if($query->execute())
-		{
-			echo "<center>Record Inserted!</center><br>";
-		}
-	}
-}
-
-if(isset($_POST["update"])){
-	if($_POST["update"]=="yes")
-	{
-		$uid=$_POST["UserID"];
-		$username=$_POST["Username"];
-		$password=$_POST["Password"];
-		$name=$_POST["Name"];
-		$role=$_POST["Role"];
-		$contact=$_POST["Contact"];	
-		$email=$_POST["Email"];
-		$creditcardid=$_POST["CreditID"];
-
-	
-	$query=$connect->prepare("update user set UserID='$uid', Username='$username', Password='$password', Name='$name',  Role='$role', Contact='$contact', Email='$email', CreditID='$creditcardid' where UserID=".$_POST['UserID']);
-		
-	//$query=$connect->prepare("update item set ITEM_NAME='$itemname' , STOCK='$stock', UNITPRICE='$unitprice', COSTPRICE='$costprice', SHORT_DESC='$shortdesc', MERCHANT='$merchant' where ITEM_ID=".$_POST['id']);
-	if($query->execute())
-	{
-		echo "<center>Record Updated!</center><br>";
-	}
-	}
-}
-
-if(isset($_GET['operation'])){
-	if($_GET['operation']=="delete")
-	{
-		$query=$connect->prepare("delete from user where UserID=".$_GET['uid']);
-		if($query->execute())
-		{
-			echo "<center>Record Deleted!</center><br>";
-		}
-	}
-}
-?>
+<!DOCTYPE html>
 <html>
+<body>
+
+<?php
+mysql_connect('localhost', 'root', '');
+
+mysql_select_db('admin');
+
+$sql="SELECT * FROM useraccount";
+
+$users= mysql_query($sql);
+
+
+?> 
     <head>
         <title>Administrator page</title>
     </head>
     <body>
 
         <fieldset>
-            <legend>Welcome <?php echo $admin->get_nicename(); ?></legend>
+            <legend>Welcome <?php echo $admin->get_name(); ?></legend>
                 <p>
                     Here are some of the basic informations
                 </p>
-                <p>
-                    Username: <?php echo $_SESSION['admin_login']; ?>
-                </p>
-                <p>
-                    Email: <?php echo $admin->get_email(); ?>
-                </p>
+				
+				<table width= "600" border="1" cellpaddin="1" cellspacing="1">
+				<tr>
+				
+				<th>Userid</th>
+				<th>Username</th>
+				<th>Password</th>
+				<th>Name</th>
+				<th>Contact</th>
+				<th>Role</th>
+				<th>Email</th>
+				<th>Address1</th>
+				<th>Address2</th>
+				<th>City</th>
+				<th>State</th>
+				<th>Postal Code</th>
+				<tr>
+				
+				<?php
+				
+				while($useraccount=mysql_fetch_assoc($users))[
+				
+					echo "<tr>";
+					
+					echo "<td>".$useraccount['userid']."</td>";
+					
+					echo "<td>".$useraccount['username']."</td>";
+					
+					echo "<td>".$useraccount['password']."</td>";
+					
+					echo "<td>".$useraccount['name']."</td>";
+					
+					echo "<td>".$useraccount['contact']."</td>";
+					
+					echo "<td>".$useraccount['role']."</td>";
+					
+					echo "<td>".$useraccount['email']."</td>";
+					
+					echo "<td>".$useraccount['address1']."</td>";
+					
+					echo "<td>".$useraccount['address2']."</td>";
+					
+					echo "<td>".$useraccount['city']."</td>";
+					
+					echo "<td>".$useraccount['state']."</td>";
+					
+					echo "<td>".$useraccount['postalcode']."</td>";
+					
+					echo "</tr>";
+				
+				]
+				?>
+				</table>
+				
         </fieldset>
         <p>
             <input type="button" onclick="javascript:window.location.href='logout.php'" value="logout" />
