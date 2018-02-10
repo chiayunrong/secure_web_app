0f3867160d04aa3c56448a681a7d1f834b617268
adriel0108 <33109096+adriel0108@users.noreply.github.com>
2018-02-10 17:46:15 +0800

    Add files via upload

---
+++ admin/login_success.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+if(!isset($_SESSION['login_user'])) //check if you are in a session, if not redirect to login page
+{ 
+   header("Location:main_login.php");
+}
+else
+{
+	header("Location:admin_home.php"); 
+}
+
+?>
