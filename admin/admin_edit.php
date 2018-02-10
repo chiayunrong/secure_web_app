23da30c43f0e8b86583e226cd2c77997893f3cc9
adriel0108 <33109096+adriel0108@users.noreply.github.com>
2018-02-10 17:43:20 +0800

    Add files via upload
    crud working, left error checking and admin role check

--- admin/admin_edit.php
+++ admin/admin_edit.php
@@ -1,41 +1,54 @@
 <html>
-<form method="post" action="admin_index.php">
+<form method="post" action="admin_home.php">
 <table align="center" border="0">
 <tr>
-<b><center>Update Record</center></b>
+<b><center>Update Account Information</center></b>
 <tr>
-<td>Username:</td>
-<td><input type="text" name="Username" value="<?php echo $_GET['Username']; ?>"/></td>
+<td>Email:</td>
+<td><input type="text" name="email" value="<?php echo $_GET['email']; ?>"/></td>
 </tr>
 <tr>
 <td>Password:</td>
-<td><input type="Password" name="Password" value="<?php echo $_GET['Password']; ?>"/></td>
+<td><input type="Password" name="password" value="<?php echo $_GET['password']; ?>"/></td>
 </tr>
 <tr>
 <td>Name:</td>
-<td><input type="text" name="Name" value="<?php echo $_GET['Name']; ?>"/></td>
+<td><input type="text" name="name" value="<?php echo $_GET['name']; ?>"/></td>
+</tr>
+<tr>
+<tr>
+<td>Contact:</td>
+<td><input type="text" name="contact" value="<?php echo $_GET['contact']; ?>"/></td>
 </tr>
 <tr>
 <td>Role:</td>	
-<td><input type="text" name="Role" value="<?php echo $_GET['Role']; ?>"/></td>
+<td><input type="text" name="role" value="<?php echo $_GET['role']; ?>"/></td>
 </tr>
 <tr>
-<td>Contact:</td>
-<td><input type="text" name="Contact" value="<?php echo $_GET['Contact']; ?>"/></td>
+<td>Address1:</td>
+<td><input type="text" name="address1" value="<?php echo $_GET['address1']; ?>"/></td>
 </tr>
 <tr>
-<td>Email:</td>
-<td><input type="text" name="Email" value="<?php echo $_GET['Email']; ?>"/></td>
+<td>Address2:</td>
+<td><input type="text" name="address2" value="<?php echo $_GET['address2']; ?>"/></td>
 </tr>
-<td>Credit ID:</td>
-<td><input type="text" name="CreditID" value="<?php echo $_GET['CreditID']; ?>"/></td>
+<tr>
+<td>City:</td>
+<td><input type="text" name="city" value="<?php echo $_GET['city']; ?>"/></td>
+</tr>
+<tr>
+<td>State:</td>
+<td><input type="text" name="state" value="<?php echo $_GET['state']; ?>"/></td>
+</tr>
+<td>Postalcode:</td>
+<td><input type="text" name="postalcode" value="<?php echo $_GET['postalcode']; ?>"/></td>
 </tr>
 <tr>
 <td>&nbsp;</td>
 <td align="right">
-<input type="hidden" name="UserID" value="<?php echo $_GET['UserID'] ?>" />
-<input type="hidden" name="update" value="yes" />
-<input type="submit" value="Update Record"/>
+<input type="hidden" name="userid" value="<?php echo $_GET['userid'] ?>" />
+<input type="hidden" name="update"value="yes"/>
+<input type="submit" value="Update Account"/>
 
 </td>
 </tr>
--- admin/admin_home.php
+++ admin/admin_home.php
@@ -1,85 +1,122 @@
+<?php
+session_start();
+include ("connect.php");
+if(isset($_POST["update"]))
+{
+	if($_POST["update"]=="yes")
+	{
+		$userid=$_POST["userid"];
+		$email=$_POST["email"];
+		$password=$_POST["password"];
+		$name=$_POST["name"];
+		$contact=$_POST["contact"];
+		$role=$_POST["role"];
+		$address1=$_POST["address1"];	
+		$address2=$_POST["address2"];
+        $city=$_POST["city"];
+		$state=$_POST["state"];
+		$postalcode=$_POST["postalcode"];	
+	
+	$query=$con->prepare("update `useraccount` set `userid`='$userid', `email`='$email' , `password`='$password', `name`='$name', `contact`='$contact', `role`='$role', `address1`='$address1', `address2`='$address2', `city`='$city', `state`='$state', `postalcode`='$postalcode' where userid=".$_POST['userid']);
+	if($query->execute())
+	{
+		echo "<center>Account Information Updated!</center><br>";
+	}
+	else
+	{
+		echo "Error updating account";
+	}
+	}	
+}
+
+if(isset($_GET['operation'])){
+	if($_GET['operation']=="delete")
+	{
+		$query=$con->prepare("delete from `useraccount` where userid=".$_GET['userid']);
+		if ($query->execute())
+					
+					{   //execute query
+  						echo "<center>Account successfully deleted.</center> <br>";
+					}
+	}
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <body>
 
-<?php
-mysql_connect('localhost', 'root', '');
 
-mysql_select_db('admin');
-
-$sql="SELECT * FROM useraccount";
-
-$users= mysql_query($sql);
-
-
-?> 
     <head>
         <title>Administrator page</title>
     </head>
+
+
     <body>
 
         <fieldset>
-            <legend>Welcome <?php echo $admin->get_name(); ?></legend>
+            <legend>Welcome Admin</legend>
                 <p>
-                    Here are some of the basic informations
+                    Here are information of users:
                 </p>
 				
-				<table width= "600" border="1" cellpaddin="1" cellspacing="1">
-				<tr>
 				
-				<th>Userid</th>
-				<th>Username</th>
-				<th>Password</th>
-				<th>Name</th>
-				<th>Contact</th>
-				<th>Role</th>
-				<th>Email</th>
-				<th>Address1</th>
-				<th>Address2</th>
-				<th>City</th>
-				<th>State</th>
-				<th>Postal Code</th>
-				<tr>
 				
-				<?php
-				
-				while($useraccount=mysql_fetch_assoc($users))[
-				
-					echo "<tr>";
-					
-					echo "<td>".$useraccount['userid']."</td>";
-					
-					echo "<td>".$useraccount['username']."</td>";
-					
-					echo "<td>".$useraccount['password']."</td>";
-					
-					echo "<td>".$useraccount['name']."</td>";
-					
-					echo "<td>".$useraccount['contact']."</td>";
-					
-					echo "<td>".$useraccount['role']."</td>";
-					
-					echo "<td>".$useraccount['email']."</td>";
-					
-					echo "<td>".$useraccount['address1']."</td>";
-					
-					echo "<td>".$useraccount['address2']."</td>";
-					
-					echo "<td>".$useraccount['city']."</td>";
-					
-					echo "<td>".$useraccount['state']."</td>";
-					
-					echo "<td>".$useraccount['postalcode']."</td>";
-					
-					echo "</tr>";
-				
-				]
-				?>
+<?php
+include("connect.php");
+
+$query = $con->prepare("SELECT * FROM useraccount");
+$query->execute();
+$query->bind_result($userid, $email, $password, $name, $contact, $role, $address1, $address2, $city, $state, $postalcode );
+echo "<table border='1'>
+<tr>
+<th>Userid</th>
+<th>Email</th>
+<th>Password</th>
+<th>Name</th>
+<th>Contact</th>
+<th>Role</th>
+<th>Address1</th>
+<th>Address2</th>
+<th>City</th>
+<th>State</th>
+<th>Postalcode</th>
+</tr>";
+
+while($query->fetch())
+{
+echo "<tr>";
+echo "<td>".$userid."</td>";
+echo "<td>".$email."</td>";
+echo "<td>".$password."</td>";
+echo "<td>".$name."</td>";
+echo "<td>".$contact."</td>";
+echo "<td>".$role."</td>";
+echo "<td>".$address1."</td>";
+echo "<td>".$address2."</td>";
+echo "<td>".$city."</td>";
+echo "<td>".$state."</td>";
+echo "<td>".$postalcode."</td>";
+echo "<td><a href='admin_edit.php?operation=edit&userid=".$userid."&email=".$email."&password=".$password."&name=".$name."&contact=".$contact."&role=".$role."&address1=".$address1."&address2=".$address2."&city=".$city."&state=".$state."&postalcode=".$postalcode."'>edit</a></td>";	
+echo "<td><a href='admin_home.php?operation=delete&userid=".$userid."'>delete</a></td>"; 
+echo "</tr>";
+}
+echo "</table>";		
+
+mysqli_close($con);
+?>	
+
+				</tr>
 				</table>
 				
         </fieldset>
         <p>
-            <input type="button" onclick="javascript:window.location.href='logout.php'" value="logout" />
+
+            <input type="button" onclick="javascript:window.location.href='admin_create.php'" value="create account"/>
+		    <input type="button" onclick="javascript:window.location.href='logout.php'" value="logout" />
+
+
         </p>
     </body>
 </html>
